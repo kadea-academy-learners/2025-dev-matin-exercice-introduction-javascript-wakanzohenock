bb313315e9a2ab6f56fd04f0e0309afe83d9d556
Chretien Kalala <chretienkalala22@gmail.com>
2025-05-14 18:57:04 +0100

    ajout des commentaires

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -5,11 +5,14 @@ let maisonFamiliale = 60000000
 let terrains = 40000000
 let liquidités = 20000000
 
+//la somme total de l'heritage
 let sommeHeritage = maisonFamiliale + terrains + liquidités
 
+//Répartition de l'heritage
 sommeTotalPremiereCategorie = sommeHeritage * 75 / 100
 sommeTotalDeuxiemeCategorie = sommeHeritage * 25 / 100
 
+//Premiere categorie
 let premierePartEnfant = sommeTotalPremiereCategorie / 3
 
 paul = premierePartEnfant
@@ -18,12 +21,14 @@ let alain = premierePartEnfant
 eric = alain / 2
 clair = alain / 2
 
+//Deuxieme categorie
 let deuxiemPartEnfant = sommeTotalDeuxiemeCategorie / 3
 
 madameMukuna = deuxiemPartEnfant
 joseph = deuxiemPartEnfant
 sarah = deuxiemPartEnfant
 
+//affichage a la console
 console.log("Paul: "+paul+" CDF")
 console.log("Marie: "+marie+" CDF")
 console.log("Eric: "+eric+" CDF")
