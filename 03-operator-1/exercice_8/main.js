4edc7fa2e59946d57bbf11b0b4fc125e0fd7e68e
Chretien Kalala <chretienkalala22@gmail.com>
2025-05-14 18:52:37 +0100

    all pass

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -7,6 +7,18 @@ let salaireMensuel,
   reste;
 // Ecrivez votre code ici
 
+salaireMensuel = 500;
+autresDepenses = 50;
+
+loyer = salaireMensuel * 30 / 100;
+nourriture = salaireMensuel * 20 / 100;
+transport = salaireMensuel * 10 / 100;
+autresDepenses += 25;
+
+totalDepenses = loyer + nourriture + transport + autresDepenses
+reste = salaireMensuel - totalDepenses
+
+
 // Ne pas modifier le code ci-dessous
 module.exports = {
   salaireMensuel,
--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,6 +1,40 @@
 let paul, marie, eric, clair, madameMukuna, joseph, sarah;
 // Ecrivez votre code ici
 
+let maisonFamiliale = 60000000
+let terrains = 40000000
+let liquidités = 20000000
+
+let sommeHeritage = maisonFamiliale + terrains + liquidités
+
+sommeTotalPremiereCategorie = sommeHeritage * 75 / 100
+sommeTotalDeuxiemeCategorie = sommeHeritage * 25 / 100
+
+let premierePartEnfant = sommeTotalPremiereCategorie / 3
+
+paul = premierePartEnfant
+marie = premierePartEnfant
+let alain = premierePartEnfant
+eric = alain / 2
+clair = alain / 2
+
+let deuxiemPartEnfant = sommeTotalDeuxiemeCategorie / 3
+
+madameMukuna = deuxiemPartEnfant
+joseph = deuxiemPartEnfant
+sarah = deuxiemPartEnfant
+
+console.log("Paul: "+paul+" CDF")
+console.log("Marie: "+marie+" CDF")
+console.log("Eric: "+eric+" CDF")
+console.log("Claire: "+clair+" CDF")
+console.log("Madame Mukuna: "+madameMukuna+" CDF")
+console.log("Joseph: "+joseph+" CDF")
+console.log("Sarah: "+sarah+" CDF")
+
+
+
+
 // Ne pas modifier le code ci-dessous
 module.exports = {
   paul,
