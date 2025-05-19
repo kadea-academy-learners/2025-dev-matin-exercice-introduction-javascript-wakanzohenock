a085a5c41bf2390695d7a3acef4c15f973c1b44f
Chretien Kalala <chretienkalala22@gmail.com>
2025-05-19 08:58:12 +0100

    update

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,7 +1,7 @@
 let ageEnfant, agePere, ageMere, ageGrandPere, ageOncle;
 // Ecrivez votre code ici
 ageEnfant = 10;
-agePere = ageEnfant + 10;
+agePere = ageEnfant * 2;
 ageMere = agePere - 5;
 ageGrandPere = (ageMere * 2) + ageEnfant/2;
 ageOncle = agePere + 10
