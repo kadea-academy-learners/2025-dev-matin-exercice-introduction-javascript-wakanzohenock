29d1444f02e0cfda1071e8fae488e39ebe93d25d
wakanzo henock <wakanzoh@gmail.com>
2025-05-26 22:12:25 +0100

    modification de quelques resolutions

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,11 +1,18 @@
-let paul = 30000000,
-  marie = 30000000,
-  eric = 15000000,
-  clair = 15000000,
-  madameMukuna = 10000000,
-  joseph = 10000000,
-  sarah = 10000000;
+let paul, marie, eric, clair, madameMukuna, joseph, sarah;
+
 // Ecrivez votre code ici
+// Première categories
+let totalPatrimoine = 60000000 + 40000000 + 20000000;
+let premiereCategorie = (75 / 100) * totalPatrimoine;
+let deuxiemeCategorie = (25 / 100) * totalPatrimoine;
+paul = (1 / 3) * premiereCategorie;
+marie = (1 / 3) * premiereCategorie;
+eric = ((1 / 3) * premiereCategorie) / 2;
+clair = ((1 / 3) * premiereCategorie) / 2;
+madameMukuna = (1 / 3) * deuxiemeCategorie;
+joseph = (1 / 3) * deuxiemeCategorie;
+sarah = (1 / 3) * deuxiemeCategorie;
+console.log(paul);
 
 // Ne pas modifier le code ci-dessous
 module.exports = {
